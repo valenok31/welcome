70b0a883fcfc0f1ae233d5ad979eee031f215a75
Andrey <priv1984@yandex.ru>
2022-09-22 00:01:28 +0700

    start project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      welcome
+        <Menus/>
+        <Menus/>
+        <Menus/>
+        <Menus/>
     </div>
   );
+}
+
+function Menus () {
+    return (
+        <div>
+            Hi!
+        </div>
+    )
 }
 
 export default App;
