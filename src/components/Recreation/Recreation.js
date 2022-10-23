e98a947beb111519f2f347bfa83c94d618a7c9eb
Andrey <priv1984@yandex.ru>
2022-10-23 23:33:58 +0700

    props.handleFetchEventsImages(r.RecAreaID);

--- src/components/Recreation/Recreation.js
+++ src/components/Recreation/Recreation.js
@@ -1,51 +1,29 @@
 import React from "react";
 import style from './Recreation.module.css'
-import axios from "axios";
 
 export default function Recreation(props) {
+    console.log(props.getEventsRecreation);
 
     let mapRequestgg = props.getEventsRecreation.map((r, i) => {
         let image = 'https://avatanplus.com/files/resources/mid/581ccfb952d8e158308b6bfb.jpg';
-/*        if (!props.getEventsRecreationImages[r.RecAreaID]) {
-            props.handleFetchEventsImages(r.RecAreaID);
-        }*/
+        /*        if (!props.getEventsRecreationImages[r.RecAreaID]) {
+                    props.handleFetchEventsImages(r.RecAreaID);
+                }*/
 
         for (let arrImages of props.getEventsRecreationImages) {
-/*            console.log(arrImages.id);
-            console.log(r.RecAreaID);*/
-            if(arrImages.id == r.RecAreaID){
+            if (arrImages.id == r.RecAreaID) {
                 console.log('excellent!!')
                 image = arrImages.url;
+            }else{
+                props.handleFetchEventsImages(r.RecAreaID);
             }
-
         }
-
 
         return <div id={i} key={i} className={style.boxMap}
                     style={{background: `url('${image}') no-repeat center/cover`}}>
             {r.RecAreaName}
         </div>
-
-
-        /*        return ()=>{
-                   return axios
-                    .get(`https://ridb.recreation.gov/api/v1/recareas/${r.RecAreaID}/media?apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
-                        .then(function (response) {
-                            //console.log(response.data.RECDATA[0].URL)
-                            return <div id={i} key={i} className={style.boxMap}
-                                        style={{background: `url(${response.data.RECDATA[0].URL}) no-repeat center/cover`}}>
-                                {r.RecAreaName}
-                            </div>
-
-                        }).catch(function (response) {
-                            //console.log("ERR")
-                            return <div id={i} key={i} className={style.boxMap}>
-                                {r.RecAreaName}
-                            </div>
-                        });
-                }*/
     })
-    //console.log(props.getEventsRecreationImages);
     return (
         <div id='fieldPlaying' className={style.field__playing}>
             {mapRequestgg}
