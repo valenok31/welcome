8947b55dc27a825c4e65e59ae2c7cfb95acc453c
Andrey <priv1984@yandex.ru>
2022-12-18 02:18:29 +0700

    Facility

--- src/components/Facility/NormalizerForFacility.js
+++ src/components/Facility/NormalizerForFacility.js
@@ -1,7 +1,60 @@
 import React from "react";
+import {connect} from "react-redux";
+import EventMainPage from "../EventMainPage/EventMainPage";
 
-const NormalizerForFacility = () => {
-    return <div>true</div>
+class NormalizerForFacility extends React.Component {
+
+    render() {
+        let id = window.location.pathname.split('/')[2];
+        let url;
+        let name = id;
+        let description = id;
+        let country = ', undefined';
+        let city = 'undefined';
+        let state = 'undefined';
+        let address = 'undefined';
+        let genre = 'undefined';
+        let segment = 'undefined';
+        let lng=0;
+        let lat=0;
+        let coordinates=[];
+
+        let result = this.props.arrayMediaFacility.find(function (item) {
+            return item.facilityId === id
+        });
+
+        if (result?.url === undefined) {
+            url = 'https://avatanplus.com/files/resources/mid/581ccfb952d8e158308b6bfb.jpg';
+        } else {
+            url = result.url;
+        }
+
+
+
+        return <>
+            <EventMainPage city={city}
+                           country={country}
+                           state={state}
+                           address={address}
+                           genre={genre}
+                           segment={segment}
+                           url={url}
+                           name={name}
+                           description={description}
+                           coordinates={coordinates}
+                           lng={lng}
+                           lat={lat}
+            />
+        </>
+    }
 }
 
-export default NormalizerForFacility;
+let mapStateToProps = (state) => {
+    return ({
+        arrayMediaFacility: state.facility_reducer.arrayMediaFacility,
+    })
+};
+
+let resultConnecting = connect(mapStateToProps, {})(NormalizerForFacility);
+
+export default resultConnecting;
