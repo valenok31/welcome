4f75f2702111ac242cd6e7fb46c020cc343440f7
Andrey <priv1984@yandex.ru>
2023-01-24 00:10:01 +0700

    Welcome

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,17 +1,33 @@
 import React from "react";
 import s from './Welcome.module.css'
+import * as axios from "axios";
 
 function Welcome(props) {
-   let  stateButton = [2,3,5,3,5,6,43,5]
-    let currentPlus = stateButton.map((r)=>{
-        return <div>
-            {r}
-        </div>
-    })
+let a={}
+
+    const options = {
+        fromFacility(limit = 5, offset = 0) {
+            const instanceRecreation = axios.create({
+                baseURL: `https://api.weatherapi.com/v1/`,
+            })
+            return instanceRecreation.get(`current.json?key=4e29dfe6fb834ea29ab152532232301&q=Moscow`)
+                .then(response => {
+                    console.log(response.data)
+                    a=response.data
+                    return response.data;
+                })
+                .catch((err) => {
+                    console.log('no data')
+                })
+        },
+    };
+
+options.fromFacility();
+
 
     return (
         <div className={s.borderBox}>
-            {currentPlus}
+            {a}
         </div>
     )
 }
