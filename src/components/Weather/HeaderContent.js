8139f91439356327f8d559d95f6f9bf459357280
Andrey <priv1984@yandex.ru>
2023-01-31 00:12:03 +0700

    onWheel, Scroll

--- src/components/Weather/HeaderContent.js
+++ src/components/Weather/HeaderContent.js
@@ -2,24 +2,62 @@ import React from "react";
 import s from "./Weather.module.css";
 
 export function HeaderContent(props) {
-
+    let k = 0;
     let temp = props.currentWeather.temp_c;
     let arr = [];
-    for (let i = 1; i < 5; i++) {
+    //for (let i = 0; i < 5; i++) {
+        function arrFunc(i){
+
+
+        let tempI = props.nextDay[i].day.avgtemp_c;
+        tempI = Math.round(tempI);
         arr.push(
-            <div className={s.content__temp_forecast}>
-                <div className={s.content__data_current}>{props.nextDay[i].date}</div>
-                {props.nextDay[i].day.avgtemp_c > 0 ? '+' : ''}{props.nextDay[i].day.avgtemp_c}°
+            /*            <div className={s.content__temp_forecast}>
+                            <div className={s.content__data_current}>{props.nextDay[i].date}</div>
+                            {props.nextDay[i].day.avgtemp_c > 0 ? '+' : ''}{props.nextDay[i].day.avgtemp_c}°
+                        </div>*/
+            <div className={s.content__current_weather}>
+                <div className={s.content__data_current}>
+                    {props.nextDay[i].date}
+                </div>
+                <div className={s.content__temp_current}>
+                    {tempI > 0 ? '+' : ''}{tempI}°
+                </div>
+                <div className={s.content__details_current}>
+                    <div className={s.details_current__parameter}>Pressure:</div>
+                    <div className={s.details_current__value}>
+                        {Math.round(props.nextDay[i].hour[12].pressure_mb * 0.750064)} mmHg
+                    </div>
+                    <div className={s.details_current__parameter}>Humidity:</div>
+                    <div className={s.details_current__value}>{Math.round(props.nextDay[i].hour[12].humidity)}%</div>
+                    <div className={s.details_current__parameter}>Wind:</div>
+                    <div
+                        className={s.details_current__value}>{Math.round(props.nextDay[i].hour[12].wind_mph * 10 / 3.6) / 10} m/s
+                        ({props.nextDay[i].hour[12].wind_dir})
+                    </div>
+                </div>
             </div>
         )
     }
 
-    function getOnWheel() {
-        console.log('scroll!')
+    function getOnWheel(e) {
+
+        if (e.deltaY < 0) {
+            if (k <= 0) k = 0
+            if (k > 0) k--
+            console.log(k)
+            arrFunc(k)
+        }
+        if (e.deltaY > 0) {
+            if (k >= 5) k = 5
+            if (k < 5) k++
+            console.log(k)
+            arrFunc(k)
+        }
     }
 
-    return <div className={s.header__content} onWheel={() => getOnWheel()}>
-        <div className={s.content__current_weather}>
+    return <div className={s.header__content} onWheel={(e) => getOnWheel(e)}>
+        {/*      <div className={s.content__current_weather}>
             <div className={s.content__data_current}>
                 {props.currentWeather.last_updated}
             </div>
@@ -28,8 +66,8 @@ export function HeaderContent(props) {
             </div>
             <div className={s.content__details_current}>
                 <div className={s.details_current__parameter}>Pressure:</div>
-                <div
-                    className={s.details_current__value}>{Math.round(props.currentWeather.pressure_mb * 0.750064)} mmHg
+                <div className={s.details_current__value}>
+                    {Math.round(props.currentWeather.pressure_mb * 0.750064)} mmHg
                 </div>
                 <div className={s.details_current__parameter}>Humidity:</div>
                 <div className={s.details_current__value}>{Math.round(props.currentWeather.humidity)}%</div>
@@ -40,9 +78,10 @@ export function HeaderContent(props) {
                 </div>
             </div>
 
-        </div>
-        <div className={s.content__forecast_weather}>
-            {arr}
-        </div>
+        </div>*/}
+        {/*        <div className={s.content__forecast_weather}>
+
+        </div>*/}
+        {arr}
     </div>
 }
--- src/components/Weather/accessoryFunctions/windDirectionStyle.js
+++ src/components/Weather/accessoryFunctions/windDirectionStyle.js
@@ -1,5 +1,5 @@
 export const windDirectionStyle = (windDegree, windKph, x) => {
-    windKph = 6000 / windKph;
+    windKph = 1000 / windKph;
     windDegree = windDegree + 90;
     return {
         transform: `rotate(${windDegree + x}deg)`,
